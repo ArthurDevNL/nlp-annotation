0e7546af590d1199d1f2280d762d61d2a121d8c4
windo <herwindo.artono@gmail.com>
2021-02-15 17:18:20 +0700

    feat: texttree - connect by ref

--- src/components/TextTree.js
+++ src/components/TextTree.js
@@ -6,8 +6,10 @@ class TextTree extends React.Component {
     constructor(props) {
         super(props);
         this.wordClick = this.wordClick.bind(this);
-        this.myRef = React.createRef();
-        console.log(this.myRef);
+        
+        // word group reference
+        this.groupRef = React.createRef();
+        this.layerRef = React.createRef();
 
         this.state = {
             sentence: "Drop the mic .",
@@ -43,6 +45,8 @@ class TextTree extends React.Component {
                 selected: [this.state.selected[0], index]
             })
             // proceed connect the line
+            // update text editor -- event
+            // reset selected
         }
     }
 
@@ -61,44 +65,70 @@ class TextTree extends React.Component {
         return index === this.state.hoveredToken;
     }
 
+    componentDidUpdate() {
+        // console.log('this.layerRef', this.layerRef);
+        // console.log('this.groupRef', this.groupRef);
+
+        // change the color of selected to white
+        if (this.layerRef.current) {
+            this.layerRef.current.children.forEach((group, index) => {
+                if (this.state.selected.includes(index + 1)) {
+                    group.children[1].fill("#ffffff");
+                } else {
+                    group.children[1].fill("#343434");
+                }
+            })
+        }
+    }
+
     render() {
         const label =
             this.props.selectedToken && this.props.selectedToken.label
                 ? this.props.selectedToken.label
                 : "None";
+        let totalTreeLength = 0;
+        totalTreeLength += window.innerWidth / 8;
         return (
             <Stage width={window.innerWidth} height={window.innerHeight/2} >
                 <Layer 
-                    ref={this.myRef}
+                    ref={this.layerRef}
                     name="text-tree-canvas" 
                     align="center">
                     {this.words.map((word, index) => {
                         index += 1;
+                        const rectLength = word.length === 1 ? word.length * 22 : word.length * 16.18;
+                        const gutter = 20;
+                        
+                        const xPosition = totalTreeLength + gutter;
+                        const yPosition = 100;
+                        totalTreeLength += rectLength + gutter;
+
                         return (
                             <Group 
+                                ref={this.groupRef}
                                 onClick={(e) => this.wordClick(e, index)}
                                 draggable={true}
                                 align="center"
-                                
+                                onMouseOver={e => this.onMouseOver(e, index)}
+                                onMouseLeave={e => this.onMouseLeave(e, index)}
                                 key={'group-' + index}>
                                 <Rect
                                     key={'rect-' + index}
-                                    x={index * 80}
-                                    y={100} 
-                                    width={word.length * 17}
+                                    x={xPosition}
+                                    y={yPosition} 
+                                    width={rectLength}
                                     height={48}
                                     stroke={this.isHovered(index) || this.state.selected.includes(index) ? '#c25e5e' : '#D2D2D2'}
                                     strokeWidth={2}
                                     cornerRadius={10}
-                                    onMouseOver={e => this.onMouseOver(e, index)}
-                                    onMouseLeave={e => this.onMouseLeave(e, index)}
+                                    
                                     fill={this.state.selected.includes(index) ? '#c25e5e' : '#D2D2D2'}
                                     align="center"
                                 />
                                 <Text 
                                     key={'text-' + index}
-                                    x={index * 80}
-                                    y={100} 
+                                    x={xPosition}
+                                    y={yPosition}
                                     height={48}
                                     verticalAlign="middle"
                                     text={word} 
